9290ba0dbf6656b784571ae9d244af09ac968f07
bakansm <khuynhbao5@gmail.com>
2022-01-21 18:01:53 +0700

    ahihi

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch
+polybar workspace &
+polybar date &
+polybar dtime &
+polybar wifi &
+polybar volume &
+polybar music &
+polybar title &
+echo "Bar launched..."
